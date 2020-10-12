9a5bada773d3350640c50536bda966a620d8c8ca
Diogo Gomes <dgomes@ua.pt>
2020-10-13 00:59:31 +0100

    add aula2

---
+++ aula2.py
@@ -0,0 +1,30 @@
+#Exercicio 4.1
+impar = None
+
+#Exercicio 4.2
+positivo = None
+
+#Exercicio 4.3
+comparar_modulo = None
+
+#Exercicio 4.4
+cart2pol = None
+
+#Exercicio 4.5
+ex5 = None
+
+#Exercicio 4.6
+def quantificador_universal(lista, f):
+    pass
+
+#Exercicio 4.9
+def ordem(lista, f):
+    pass
+
+#Exercicio 4.10
+def filtrar_ordem(lista, f):
+    pass
+
+#Exercicio 5.2
+def ordenar_seleccao(lista, ordem):
+    pass
