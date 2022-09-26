6a6f6379d8c20e2070523733349b9f67a8aa98e8
Luis Seabra Lopes <lsl@ua.pt>
2022-09-26 14:34:55 +0100

    minor

--- tests/test_aula1.py
+++ tests/test_aula1.py
@@ -44,9 +44,9 @@ def test_capicua(mock_capicua):
 
 #Exercicio 1.7
 @mock.patch('aula1.concat_listas', side_effect = aula1.concat_listas)
-def test_explode(mock_explode):
-    assert mock_explode([[1,2], [3,4]]) == [1,2,3,4]
-    assert mock_explode([[1,2], [3,4], [5]]) == [1,2,3,4,5]
+def test_concat_listas(mock_explode):
+    assert mock_concat_listas([[1,2], [3,4]]) == [1,2,3,4]
+    assert mock_concat_listas([[1,2], [3,4], [5]]) == [1,2,3,4,5]
 
 #Exercicio 1.8
 @mock.patch('aula1.substitui', side_effect = aula1.substitui)
