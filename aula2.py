15971843cc5efb31e8c68848bbb5eb6e9ba5c5fd
Diogo Gomes <diogogomes@gmail.com>
2022-09-28 13:34:46 +0100

    Update aula2.py

--- aula2.py
+++ aula2.py
@@ -15,51 +15,20 @@ ex5 = None
 
 #Exercicio 4.6
 def quantificador_universal(lista, f):
-    if lista == []:
-        return True
-
-    if f(lista[0]):
-        return quantificador_universal(lista[1:], f)
-    return False
+    pass
 
 def subconjunto(lista1, lista2):
-    if lista1 == []:
-        return True
-
-    if lista1[0] in lista2:
-        return subconjunto(lista1[1:], lista2)
-    return False
-
-print(subconjunto([0,1,2,3], [3,4,2,1]))
+    pass
 
 #Exercicio 4.9
 def ordem(lista, f):
-    if len(lista) == 1:
-        return lista[0]
-
-    menor = ordem(lista[1:], f)
-
-    if f(lista[0], menor):
-        return lista[0]
-    return menor
+    pass
 
 #Exercicio 4.10
 def filtrar_ordem(lista, f):
-    if len(lista) == 1:
-        return lista[0], []
-
-    menor, resto = filtrar_ordem(lista[1:], f)
-
-    if f(lista[0], menor):
-        return lista[0], resto + [menor]
-    return menor, [lista[0]] + resto
+    pass
 
 #Exercicio 5.2
 def ordenar_seleccao(lista, ordem):
-    if lista == []:
-        return []
-
-    menor, resto = filtrar_ordem(lista, ordem)
-
-    return [menor] + ordenar_seleccao(resto, ordem)
+    pass
 
