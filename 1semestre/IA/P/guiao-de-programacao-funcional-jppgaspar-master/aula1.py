eb48046630de0f483d6d6fc41c7b7300327b62ca
João Gaspar <joaogaspar003@gmail.com>
2023-09-21 20:09:13 +0100

    ia

--- 1semestre/IA/P/guiao-de-programacao-funcional-jppgaspar-master/aula1.py
+++ 1semestre/IA/P/guiao-de-programacao-funcional-jppgaspar-master/aula1.py
@@ -29,11 +29,19 @@ def concat(l1, l2):
 
 #Exercicio 1.5
 def inverte(lista):
-	pass
+	if len(lista) == 0:
+		return []
+	else:
+		return [lista[-1]] + inverte(lista[:-1])
 
 #Exercicio 1.6
 def capicua(lista):
-	pass
+	if len(lista) <= 1:
+		return True
+	elif lista[0] == lista[-1]:
+		return capicua(lista[1:-1])
+	else:
+		return False
 
 #Exercicio 1.7
 def concat_listas(lista):
@@ -44,17 +52,29 @@ def concat_listas(lista):
 
 #Exercicio 1.8
 def substitui(lista, original, novo):
-	pass
+	if lista == []:
+		return []
+	if lista[0] == original:
+		return [novo] + substitui(lista[1:],original,novo)
+	else:
+		return [lista[0]] + substitui(lista[1:],original,novo)
+
 
 #Exercicio 1.9
 def fusao_ordenada(lista1, lista2):
-    
-    
+	if lista2 == []:
+		return lista1
+	if lista1 == []:
+		return lista2
+	if lista1[0] < lista2[0]:
+		return [lista1[0]] + fusao_ordenada(lista1[1:],lista2)
+	return [lista2[0]] + fusao_ordenada(lista1,lista2[1:])
 
 #Exercicio 1.10
 def lista_subconjuntos(lista):
-	pass
-
+	if lista == []:
+		return []
+	return [lista[0]] + lista_subconjuntos(lista[1:])
 
 #Exercicio 2.1
 def separar(lista):
