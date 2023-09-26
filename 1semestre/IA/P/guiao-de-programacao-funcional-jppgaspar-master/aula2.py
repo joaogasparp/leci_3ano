1c4a91378fc95a7460c3f32433ff791390ed7f2f
João Gaspar <joaogaspar003@gmail.com>
2023-09-26 15:59:25 +0100

    ia

--- 1semestre/IA/P/guiao-de-programacao-funcional-jppgaspar-master/aula2.py
+++ 1semestre/IA/P/guiao-de-programacao-funcional-jppgaspar-master/aula2.py
@@ -1,33 +1,49 @@
 #Exercicio 4.1
-impar = None
+impar = lambda x: True if x % 2 == 1 else False
 
 #Exercicio 4.2
-positivo = None
+positivo = lambda x: True if x>0 else False
 
 #Exercicio 4.3
-comparar_modulo = None
+comparar_modulo = lambda x,y: True if abs(x)<abs(y) else False
 
 #Exercicio 4.4
-cart2pol = None
+import math
+cart2pol = lambda x,y: (math.sqrt(x*x + y*y), math.atan2(y, x))
 
 #Exercicio 4.5
-ex5 = None
+ex5 = lambda f,g,h: lambda x,y,z: h(f(x,y),g(y,z))
 
 #Exercicio 4.6
 def quantificador_universal(lista, f):
-    pass
+    if lista == []:
+        return True
+    return f(lista[0]) and quantificador_universal(lista[1:], f)
 
 #Exercicio 4.8
 def subconjunto(lista1, lista2):
-    pass
+    if lista1 == []:
+        return True
+    if lista1[0] in lista2:
+        return subconjunto(lista1[1:], lista2)
+    else:
+        return False
 
 #Exercicio 4.9
 def menor_ordem(lista, f):
-    pass
+    if len(lista) == 1:
+        return lista[0]
+    min = menor_ordem(lista[1:], f)
+    if f(lista[0],min):
+        return lista[0]
+    else:
+        return min
 
 #Exercicio 4.10
 def menor_e_resto_ordem(lista, f):
-    pass
+    min = menor_ordem(lista,f)
+    resto = lista.remove(min)
+    return min,resto
 
 #Exercicio 5.2
 def ordenar_seleccao(lista, ordem):
