e19ca6ec61ad45507cbb4efa1d700c2095980e86
João Gaspar <j.gaspar@ua.pt>
2023-11-30 11:39:12 +0000

    ia

--- 1semestre/IA/P/guiao-sobre-pesquisa-joaogasparp-master/amigos.py
+++ 1semestre/IA/P/guiao-sobre-pesquisa-joaogasparp-master/amigos.py
@@ -2,6 +2,14 @@ from constraintsearch import *
 
 amigos = ["Andre", "Bernardo", "Claudio"]
 
-cs = ConstraintSearch(None, None)
+dominios = { 'Andre': [ ('Bernando', 'Claudio'), ('Claudio', 'Bernardo') ],
+             'Bernardo': [ ('Andre', 'Claudio'), ('Claudio', 'Andre') ],
+             'Claudio': [ ('Andre', 'Bernardo'), ('Bernardo', 'Andre') ] }
+
+arestas = [ (a1, a2) for a1 in amigos for a2 in amigos if a1 != a2 ]
+
+restricoes = { a: lambda v1,x1,v2,x2: x1[1]}
+
+cs = ConstraintSearch(dominios, None)
 
 print(cs.search())
--- 1semestre/IA/P/guiao-sobre-pesquisa-joaogasparp-master/mapas.py
+++ 1semestre/IA/P/guiao-sobre-pesquisa-joaogasparp-master/mapas.py
@@ -3,6 +3,14 @@ from constraintsearch import *
 region = ['A', 'B', 'C', 'D', 'E']
 colors = ['red', 'blue', 'green', 'yellow', 'white']
 
-cs = ConstraintSearch(None, None)
+dominios = { r:colors for r in region }
+
+arestas = [ ('A','B'), ('B','C'), ('C','D'), ('D','A') ]
+arestas += [ (r, 'E') for r in region if r != 'E' ]
+arestas += [ (v2,v1) for (v1,v2) in arestas ]
+
+restrições = { (v1,v2): lambda r1,c1,r2,c2: c1!=c2 for (v1,v2) in arestas }
+
+cs = ConstraintSearch(dominios,restrições)
 
 print(cs.search())
