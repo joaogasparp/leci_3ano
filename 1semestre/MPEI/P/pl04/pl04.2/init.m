1e2654bf0e79d5844dbb3aa4e9467e4d9b3e2d40
João Gaspar <j.gaspar@ua.pt>
2023-12-19 18:33:22 +0000

    mpei

--- 1semestre/MPEI/P/pl04/pl04.2/init.m
+++
@@ -1,3 +0,0 @@
-function BF = init(n)
-    BF = false(1,n);
-end
--- 1semestre/MPEI/P/pl04/pl04.2/insert.m
+++
@@ -1,9 +0,0 @@
-function BF = insert(elemento, BF, k)
-    n = length(BF);
-    for i = 1:k
-        elemento = [elemento num2str(i)];
-        h = DJB31MA(elemento, 127);
-        h = mod(h,n) + 1;
-        BF(h) = true;
-    end
-end
