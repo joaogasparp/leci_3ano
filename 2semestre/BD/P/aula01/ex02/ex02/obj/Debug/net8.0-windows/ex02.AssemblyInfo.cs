c19bc54a10164c774c73b137ca2a4bf13f289721
João Gaspar <j.gaspar@ua.pt>
2024-05-29 15:23:36 +0100

    bd

--- 2semestre/BD/P/aula01/ex02/ex02/obj/Debug/net8.0-windows/ex02.AssemblyInfo.cs
+++ 2semestre/BD/P/aula01/ex02/ex02/obj/Debug/net8.0-windows/ex02.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ex02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a2c0993fa233658b16f33e9fdeaf53a06ea3a8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b41941ed53c25651433b451283585925ddc8d03")]
 [assembly: System.Reflection.AssemblyProductAttribute("ex02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ex02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 2semestre/BD/P/projeto/APFT_107708_107961/source/form/LoginPage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace form
+{
+    public partial class LoginPage : Form
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void passe_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((utilizador.Text == "area1" && passe.Text == "gerente1") || (utilizador.Text == "area2" && passe.Text == "gerente2") || (utilizador.Text == "area3" && passe.Text == "gerente3") || (utilizador.Text == "area4" && passe.Text == "gerente4"))
+            {
+                AreaServicoPage areaServicoPage = new AreaServicoPage();
+                this.Hide();
+                areaServicoPage.Show();
+            }
+            else
+            {
+                MessageBox.Show("Credenciais inválidas. Tente novamente.");
+            }
+        }
+    }
+}
