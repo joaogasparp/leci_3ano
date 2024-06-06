c2b515d67e5b50efea3077c56179000c342ced12
João Gaspar <j.gaspar@ua.pt>
2024-06-07 00:39:25 +0100

    bd

---
+++ 2semestre/BD/P/projeto/APFT_107708_107961/code/form/DataBaseConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace form
+{
+    public static class DatabaseConnection
+    {
+        private static string dataSource = "tcp:mednat.ieeta.pt\\SQLSERVER,8101";
+        private static string initialCatalog = "p1g8";
+        private static string uid = "p1g8";
+        private static string password = "IrineuSonic28g*";
+
+        public static string connectionString = $"Data Source = {dataSource}; Initial Catalog = {initialCatalog}; uid= {uid}; password = {password}";
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
